3617280543643ac445866a49f4e9efd28bf6a06a
bpking1 <yihe.hejie@gmail.com>
2025-03-12 16:23:28 +0800

    update

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -18,6 +18,21 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    -- extras
+    { import = "lazyvim.plugins.extras.lang.omnisharp" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    -- { import = "lazyvim.plugins.extras.lang.docker" },
+    -- { import = "lazyvim.plugins.extras.linting.eslint" },
+    -- { import = "lazyvim.plugins.extras.lang.go" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.lang.python" },
+    -- { import = "lazyvim.plugins.extras.lang.rust" },
+    -- { import = "lazyvim.plugins.extras.lang.tailwind" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.yaml" },
+    -- { import = "lazyvim.plugins.extras.coding.yanky" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
--- private_dot_config/nvim/lua/plugins/markdown.lua
+++ private_dot_config/nvim/lua/plugins/markdown.lua
@@ -1,26 +1,25 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-  -- 用上面这个官方默认的配置就好了，需要改的话再覆写
-  --   {
-  --     "iamcco/markdown-preview.nvim",
-  --     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --     keys = {
-  --       { "<leader>cp", "<cmd>MarkdownPreview<cr>", desc = "MarkdownPreview" },
-  --     },
-  --     ft = { "markdown" },
-  --     build = function()
-  --       vim.fn["mkdp#util#install"]()
-  --     end,
-  --   },
-  --   每个语言的lsp配置写在一起好点，不用专门一个lspconfig.lua文件
-  --   {
-  --     "neovim/nvim-lspconfig",
-  --     ---@class PluginLspOpts
-  --     opts = {
-  --       ---@type lspconfig.options
-  --       servers = {
-  --         marksman = {},
-  --       },
-  --     },
-  --   },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = false,
+      },
+    },
+  },
+
+  { "iamcco/markdown-preview.nvim", enabled = false },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = function(_, opts)
+      return {
+        code = {
+          sign = false,
+          width = "block",
+          right_pad = 1,
+        },
+      }
+    end,
+  },
 }
